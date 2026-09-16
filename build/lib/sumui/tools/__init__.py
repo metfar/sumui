c850787d39c56a8fea9c0e18bf9b669313c8993b
metfar <metfar@gmail.com>
2026-09-16 11:40:26 -0300

    Update sumui-0.1.0a16

---
+++ build/lib/sumui/tools/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#pylint:disable=W0301
+#  
+#  Copyright 2018- William Martinez Bas <metfar@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
